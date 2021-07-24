077fcccf8905a82d06f194b89f4b6bc6ecac4e60
megmon <june.milkywa.d@gmail.com>
2021-07-24 09:30:22 +0900

    FEDay5課題（環境構築）

---
+++ src/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports={
+    env:{
+        browser: true,
+        es6: true
+    },
+    extends:[
+        "eslint:recommended",
+        "prettier",
+    ],
+    Plugins:[
+
+    ],
+    parser:{},
+    parserOptions:{},
+    root: true,
+    rules:{}
+}
